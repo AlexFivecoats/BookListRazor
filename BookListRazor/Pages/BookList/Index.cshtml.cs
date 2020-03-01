7ab798b10d299d746d247c0e2564bfa9acfddb39
Alex Fivecoats <ultramegacoats@gmail.com>
2020-03-01 13:49:16 -0500

    DataTable implemented Delete api call functioning

--- BookListRazor/Pages/BookList/Index.cshtml.cs
+++ BookListRazor/Pages/BookList/Index.cshtml.cs
@@ -11,7 +11,6 @@ namespace BookListRazor.Pages.BookList
 {
     public class IndexModel : PageModel
     {
-
         private readonly ApplicationDbContext _db;
 
         public IndexModel(ApplicationDbContext db)
@@ -24,6 +23,19 @@ namespace BookListRazor.Pages.BookList
         public async Task OnGet()
         {
             Books = await _db.Book.ToListAsync();
+        }
+
+        public async Task<IActionResult> OnPostDelete(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _db.Book.Remove(book);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
